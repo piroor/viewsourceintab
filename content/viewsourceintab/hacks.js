b33f385171f12f59c90fb05d960125272951ca7d
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-10-31 05:23:33 +0000

    reduce indent
    git-svn-id: https://www.cozmixng.org/repos/piro/viewsourceintab/trunk@5289 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/viewsourceintab/hacks.js
+++ content/viewsourceintab/hacks.js
@@ -2,12 +2,10 @@ ViewSourceInTab.overrideExtensionsPreInit = function() {
 
 	// Highlander
 	if ('Highlander' in window) {
-		eval('Highlander.overrideHandleLinkClick = '+
-			Highlander.overrideHandleLinkClick.toSource().replace(
-				/(var )?origHandleLinkClick/g,
-				'window.__viewsourceintab__highlander__origHandleLinkClick'
-			)
-		);
+		eval('Highlander.overrideHandleLinkClick = '+Highlander.overrideHandleLinkClick.toSource().replace(
+			/(var )?origHandleLinkClick/g,
+			'window.__viewsourceintab__highlander__origHandleLinkClick'
+		));
 	}
 
 };
