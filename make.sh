eee7672b1b6d5b2725727aea67d07009d850643d
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-11-08 16:01:32 +0900

    buildscript submodule

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp buildscript/make_new.sh ./
-./make_new.sh viewsourceintab version=0
-rm make_new.sh
+cp buildscript/makexpi.sh ./
+./makexpi.sh viewsourceintab version=0
+rm makexpi.sh
 
